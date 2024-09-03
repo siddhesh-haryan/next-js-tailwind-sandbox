b63a7fc551d2abf6d157973fd56d7248e9706721
siddhesh haryan <siddhesharyan876@gmail.com>
2024-09-03 10:58:12 +0530

    First Commit

---
+++ next-js-tailwind-sandbox/app/page.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function page() {
+  return (
+    <div>
+      <div>page</div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio ullam
+        adipisci quos ab nemo dolores laboriosam animi iusto assumenda. Et vitae
+        reprehenderit dolores eligendi vero error voluptates assumenda vel,
+        porro perspiciatis culpa?
+      </p>
+    </div>
+  );
+}
+
+export default page;
